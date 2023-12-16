f446eac5cd33a985a17a5d1b22bdaa4a1c07bccf
Periklismant <periklismant1@gmail.com>
2023-12-16 20:20:16 +0200

    netbill fi experiments

--- systems/sCASP/event_descriptions/netbillfi.pl
+++ systems/sCASP/event_descriptions/netbillfi.pl
@@ -6,6 +6,7 @@
 %% Grounding of Entities %% 
 goods(book).
 price(10).
+delay(5).
 
 %% Domain Knowledge %%
 initiates(present_quote(M,C,GD,P), quote_true(M,C,GD), T):-
@@ -18,4 +19,4 @@ terminates(accept_quote(C,M,GD), quote_false(M,C,GD), T):-
 terminates(accept_quote(C,M,GD), quote_true(M,C,GD), T):-
 	merchant(M), consumer(C), goods(GD).
 
-trajectory(quote_true(M,C,GD), T1, quote_false(M,C,GD), T2) :- delay(R), T2 .>=. T1 + R.
+trajectory(quote_true(M,C,GD), T1, quote_false(M,C,GD), T2) :- delay(R), T2 .>=. T1 + R, merchant(M), consumer(C), goods(GD).
